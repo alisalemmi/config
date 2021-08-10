90bcebaac89ad1e5bdd030b0a32471df58e742d6
علی سالمی <45937252+alisalemmi@users.noreply.github.com>
2021-08-10 16:47:04 +0430

    powershell در تاریخچه cursor حل مشکل مکان

--- shell/powershell.ps1
+++ shell/powershell.ps1
@@ -1,8 +1,9 @@
 $env:POSH_GIT_ENABLED = $true
-Set-PoshPrompt -Theme C:\Users\abbas\AppData\Local\pwsh\theme.json
+Set-PoshPrompt -Theme C:\Users\<user-name>\AppData\Local\pwsh\theme.json
 
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 
 # Autocompletion for arrow keys
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
