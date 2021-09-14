523719a70d51b3e9f461a53de95e37c17476e535
علی سالمی <45937252+alisalemmi@users.noreply.github.com>
2021-09-14 08:53:12 +0430

    به کلمه بعدی پیشنهاد می رود ctrl + ->

--- shell/powershell.ps1
+++ shell/powershell.ps1
@@ -4,6 +4,7 @@ Set-PoshPrompt -Theme C:\Users\<user-name>\AppData\Local\pwsh\theme.json
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
 
 # Autocompletion for arrow keys
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
